25b69c35ae2efb1be1f72c6f09c2af80730acf35
Jacopo Chevallard <jacopo.chevallard@gmail.com>
2018-04-09 11:38:21 +0200

    Modified to work on list of Beagle files

--- bin/add_EWs_to_beagle_output.py
+++ bin/add_EWs_to_beagle_output.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
         help="Name of the Beagle file containing the model SEDs for which EWs must be computed.",
         action="store", 
         type=str, 
+        nargs="+",
         dest="beagle_file", 
         required=True
     )
@@ -39,111 +40,113 @@ if __name__ == '__main__':
     with open(args.json_file) as f:
         lines = json.load(f, object_pairs_hook=OrderedDict)
     
-    hdulist = fits.open(args.beagle_file) 
-    wl = np.ravel(hdulist['FULL SED WL'].data[0][:])
-    n_rows = len(hdulist[1].data.field(1))
+    for f in args.beagle_file:
 
-    # Create an empty dictionary of numpy array which will containe the EWs
-    EWs = OrderedDict()
-    integrated_fluxes = OrderedDict()
-    for line in lines:
-        EWs[line] = np.zeros(n_rows, dtype=np.float32)
+        hdulist = fits.open(f) 
+        wl = np.ravel(hdulist['FULL SED WL'].data[0][:])
+        n_rows = len(hdulist[1].data.field(1))
 
-    SEDs = hdulist['FULL SED'].data[:,:]
+        # Create an empty dictionary of numpy array which will containe the EWs
+        EWs = OrderedDict()
+        integrated_fluxes = OrderedDict()
+        for line in lines:
+            EWs[line] = np.zeros(n_rows, dtype=np.float32)
 
-    # Cycle across all the lines for which we compute the EWs
-    for key, value in lines.iteritems():
+        SEDs = hdulist['FULL SED'].data[:,:]
 
-        # Compute the average left continuum
-        il0 = np.searchsorted(wl, value["continuum_left"][0]) ; il0 -= 1
-        il1 = np.searchsorted(wl, value["continuum_left"][1])
-        if il0 == il1:
-            il0 -= 1
-        flux_left = np.ravel(np.trapz(SEDs[:,il0:il1+1], x=wl[il0:il1+1], axis=1)) / (wl[il1]-wl[il0])
-        wl_left = 0.5*(wl[il0]+wl[il1])
+        # Cycle across all the lines for which we compute the EWs
+        for key, value in lines.iteritems():
 
-        # Compute the average right continuum
-        ir0 = np.searchsorted(wl, value["continuum_right"][0]) ; ir0 -= 1
-        ir1 = np.searchsorted(wl, value["continuum_right"][1])
-        if ir0 == ir1:
-            ir0 -= 1
-        flux_right = np.ravel(np.trapz(SEDs[:,ir0:ir1+1], x=wl[ir0:ir1+1], axis=1)) / (wl[ir1]-wl[ir0])
-        wl_right = 0.5*(wl[ir0]+wl[ir1])
+            # Compute the average left continuum
+            il0 = np.searchsorted(wl, value["continuum_left"][0]) ; il0 -= 1
+            il1 = np.searchsorted(wl, value["continuum_left"][1])
+            if il0 == il1:
+                il0 -= 1
+            flux_left = np.ravel(np.trapz(SEDs[:,il0:il1+1], x=wl[il0:il1+1], axis=1)) / (wl[il1]-wl[il0])
+            wl_left = 0.5*(wl[il0]+wl[il1])
 
-        # Approximate the continuum with a straght line
-        grad = (flux_right-flux_left)/(wl_right-wl_left)
-        intercept = flux_right - grad*wl_right
-    
-        #### Compute EW
-        i0 = np.searchsorted(wl, value["wl_range"][0]) ; i0 -= 1
-        i1 = np.searchsorted(wl, value["wl_range"][1])
-        n_wl = i1-i0+1
+            # Compute the average right continuum
+            ir0 = np.searchsorted(wl, value["continuum_right"][0]) ; ir0 -= 1
+            ir1 = np.searchsorted(wl, value["continuum_right"][1])
+            if ir0 == ir1:
+                ir0 -= 1
+            flux_right = np.ravel(np.trapz(SEDs[:,ir0:ir1+1], x=wl[ir0:ir1+1], axis=1)) / (wl[ir1]-wl[ir0])
+            wl_right = 0.5*(wl[ir0]+wl[ir1])
 
-        # Repeat the wl array along the 0 axis to be able to run the algorithms over all rows at the same time
-        wl_repeat = np.repeat(wl[np.newaxis, i0:i1+1], n_rows, axis=0)
+            # Approximate the continuum with a straght line
+            grad = (flux_right-flux_left)/(wl_right-wl_left)
+            intercept = flux_right - grad*wl_right
+        
+            #### Compute EW
+            i0 = np.searchsorted(wl, value["wl_range"][0]) ; i0 -= 1
+            i1 = np.searchsorted(wl, value["wl_range"][1])
+            n_wl = i1-i0+1
 
-        # Interpolate the SED at the edges to have to right integration limits
-        SED = np.copy(SEDs[:,i0:i1+1])
-        SED[:,0] = SED[:,0] + (SED[:,1]-SED[:,0])/(wl_repeat[:,1]-wl_repeat[:,0]) * (value["wl_range"][0]-wl_repeat[:,0])
-        SED[:,-1] = SED[:,-2] + (SED[:,-2]-SED[:,-1])/(wl_repeat[:,-2]-wl_repeat[:,-1]) * (value["wl_range"][1]-wl_repeat[:,-2])
+            # Repeat the wl array along the 0 axis to be able to run the algorithms over all rows at the same time
+            wl_repeat = np.repeat(wl[np.newaxis, i0:i1+1], n_rows, axis=0)
 
-        # Build the actual wl array over which you will perform the integration
-        wl_ = np.zeros(n_wl)
-        wl_[0] = value["wl_range"][0]
-        wl_[-1] = value["wl_range"][1]
-        wl_[1:-1] = wl[i0+1:i1]
-        wl_ = np.repeat(wl_[np.newaxis,:], n_rows, axis=0)
+            # Interpolate the SED at the edges to have to right integration limits
+            SED = np.copy(SEDs[:,i0:i1+1])
+            SED[:,0] = SED[:,0] + (SED[:,1]-SED[:,0])/(wl_repeat[:,1]-wl_repeat[:,0]) * (value["wl_range"][0]-wl_repeat[:,0])
+            SED[:,-1] = SED[:,-2] + (SED[:,-2]-SED[:,-1])/(wl_repeat[:,-2]-wl_repeat[:,-1]) * (value["wl_range"][1]-wl_repeat[:,-2])
 
-        # Linear function approximating the continuum
-        grad_ = np.repeat(grad[:, np.newaxis], n_wl, axis=1)
-        intercept_ = np.repeat(intercept[:, np.newaxis], n_wl, axis=1)
-    
-        integrand = 1.0 - SED/(grad_*wl_+intercept_)
-        EW = np.ravel(np.trapz(integrand, x=wl_, axis=1))
-        EWs[key] = -EW
+            # Build the actual wl array over which you will perform the integration
+            wl_ = np.zeros(n_wl)
+            wl_[0] = value["wl_range"][0]
+            wl_[-1] = value["wl_range"][1]
+            wl_[1:-1] = wl[i0+1:i1]
+            wl_ = np.repeat(wl_[np.newaxis,:], n_rows, axis=0)
 
-        integrand = SED-(grad_*wl_+intercept_)
-        integrated_flux = np.ravel(np.trapz(integrand, x=wl_, axis=1))
-        integrated_fluxes[key] = integrated_flux
+            # Linear function approximating the continuum
+            grad_ = np.repeat(grad[:, np.newaxis], n_wl, axis=1)
+            intercept_ = np.repeat(intercept[:, np.newaxis], n_wl, axis=1)
+        
+            integrand = 1.0 - SED/(grad_*wl_+intercept_)
+            EW = np.ravel(np.trapz(integrand, x=wl_, axis=1))
+            EWs[key] = -EW
+
+            integrand = SED-(grad_*wl_+intercept_)
+            integrated_flux = np.ravel(np.trapz(integrand, x=wl_, axis=1))
+            integrated_fluxes[key] = integrated_flux
 
 
-    # Create a list of columns from the dictionary containing the EWs
-    cols = list()
-    for key, value in EWs.iteritems():
-        col = fits.Column(name=str(key), format='E', array=value)
-        cols.append(col)
+        # Create a list of columns from the dictionary containing the EWs
+        cols = list()
+        for key, value in EWs.iteritems():
+            col = fits.Column(name=str(key), format='E', array=value)
+            cols.append(col)
 
-    # Create a new binary table HDU 
-    columns = fits.ColDefs(cols)
-    new_hdu = fits.BinTableHDU.from_columns(columns)
-    new_hdu.name = 'EQUIVALENT WIDTHS'
+        # Create a new binary table HDU 
+        columns = fits.ColDefs(cols)
+        new_hdu = fits.BinTableHDU.from_columns(columns)
+        new_hdu.name = 'EQUIVALENT WIDTHS'
 
-    # Add the new HDU to the Beagle file
-    if new_hdu.name in hdulist:
-        hdulist[new_hdu.name] = new_hdu
-    else:
-        hdulist.append(new_hdu)
+        # Add the new HDU to the Beagle file
+        if new_hdu.name in hdulist:
+            hdulist[new_hdu.name] = new_hdu
+        else:
+            hdulist.append(new_hdu)
 
-    # Create a list of columns from the dictionary containing the integrated fluxes
-    cols = list()
-    for key, value in integrated_fluxes.iteritems():
-        col = fits.Column(name=str(key), format='E', array=value)
-        cols.append(col)
+        # Create a list of columns from the dictionary containing the integrated fluxes
+        cols = list()
+        for key, value in integrated_fluxes.iteritems():
+            col = fits.Column(name=str(key), format='E', array=value)
+            cols.append(col)
 
-    # Create a new binary table HDU 
-    columns = fits.ColDefs(cols)
-    new_hdu = fits.BinTableHDU.from_columns(columns)
-    new_hdu.name = 'INTEGRATED FLUXES'
+        # Create a new binary table HDU 
+        columns = fits.ColDefs(cols)
+        new_hdu = fits.BinTableHDU.from_columns(columns)
+        new_hdu.name = 'INTEGRATED FLUXES'
 
-    # Add the new HDU to the Beagle file
-    if new_hdu.name in hdulist:
-        hdulist[new_hdu.name] = new_hdu
-    else:
-        hdulist.append(new_hdu)
+        # Add the new HDU to the Beagle file
+        if new_hdu.name in hdulist:
+            hdulist[new_hdu.name] = new_hdu
+        else:
+            hdulist.append(new_hdu)
 
-    hdulist.writeto(args.beagle_file, clobber=True)
+        hdulist.writeto(f, overwrite=True)
 
-    hdulist.close()
+        hdulist.close()
 
 
 
--- bin/add_UV_slope.py
+++ bin/add_UV_slope.py
@@ -16,76 +16,79 @@ if __name__ == '__main__':
 
     parser.add_argument(
         '--beagle-file',
-        help="Name of the Beagle file containing the model SEDs for which EWs must be computed.",
+        help="Name of the Beagle output file(s).",
         action="store", 
         type=str, 
+        nargs="+",
         dest="beagle_file", 
         required=True
     )
 
     args = parser.parse_args()
 
-    cat =  fits.open(args.beagle_file, mode='update')
-    
-    binArr = np.array([[1268.,1284.], \
-          [1309.,1316.], \
-          [1342.,1371.], \
-          [1407.,1515.], \
-          [1562.,1583.], \
-          [1677.,1740.], \
-          [1760.,1833.], \
-          [1866.,1890.], \
-          [1930.,1950.], \
-          [2400.,2580.]])
+    for f in args.beagle_file:
 
-    bin_wl = np.zeros(10)
-    for i in range(10):
-        bin_wl[i] = np.mean(binArr[i])
+        cat =  fits.open(f, mode='update')
         
-    wl = cat['FULL SED WL'].data[0][0]
+        binArr = np.array([[1268.,1284.], \
+              [1309.,1316.], \
+              [1342.,1371.], \
+              [1407.,1515.], \
+              [1562.,1583.], \
+              [1677.,1740.], \
+              [1760.,1833.], \
+              [1866.,1890.], \
+              [1930.,1950.], \
+              [2400.,2580.]])
 
-    #extend wavelength array to include bin boundaries
-    binIdx = 0
-    wl_ext = []
-    for i in range(len(wl)-1):
-        while binArr[binIdx,1] < wl[i]:
-            if binIdx < len(binArr)-1:
-                binIdx = binIdx + 1
-            if binIdx == 9:
-                break
-        wl_ext.append(wl[i])
-        if binIdx < len(binArr):
-            if wl[i] < binArr[binIdx,0] and wl[i+1] > binArr[binIdx,0]:
-                wl_ext.append(binArr[binIdx,0])
-            if wl[i] < binArr[binIdx,1] and wl[i+1] > binArr[binIdx,1]:
-                wl_ext.append(binArr[binIdx,1])
-   
+        bin_wl = np.zeros(10)
+        for i in range(10):
+            bin_wl[i] = np.mean(binArr[i])
+            
+        wl = cat['FULL SED WL'].data[0][0]
 
-    wl_ext.append(wl[-1])
-    wl_ext = np.array(wl_ext)
-    
-    nObj = len(cat['FULL SED'].data[:,0])
-    
-    uv_slope = np.zeros(nObj)
-    for i in range(nObj):
-        spec_flambda = cat['FULL SED'].data[i,:]
+        #extend wavelength array to include bin boundaries
+        binIdx = 0
+        wl_ext = []
+        for i in range(len(wl)-1):
+            while binArr[binIdx,1] < wl[i]:
+                if binIdx < len(binArr)-1:
+                    binIdx = binIdx + 1
+                if binIdx == 9:
+                    break
+            wl_ext.append(wl[i])
+            if binIdx < len(binArr):
+                if wl[i] < binArr[binIdx,0] and wl[i+1] > binArr[binIdx,0]:
+                    wl_ext.append(binArr[binIdx,0])
+                if wl[i] < binArr[binIdx,1] and wl[i+1] > binArr[binIdx,1]:
+                    wl_ext.append(binArr[binIdx,1])
+       
 
-        #calculate mean flambda in each window
-        #if the wavelengths for the bin edges are not in the wavelength
-        #array, first resample to include them
-        spec_ext_flambda = np.interp(wl_ext, wl, spec_flambda)
-        flambda_windows = np.zeros(10)
-        for j in range(10):
-            flambda_windows[j] = np.mean(spec_ext_flambda[np.all([wl_ext >= binArr[j,0], wl_ext <= binArr[j,1]],axis=0)])
-
-        #fit straight line to these fluxes to derive the UV slope
-
-        a, b, sa, sb, rchi2, dof=lf.linear_fit(np.log10(bin_wl), np.log10(flambda_windows),silent=True)
-        uv_slope[i] = a
+        wl_ext.append(wl[-1])
+        wl_ext = np.array(wl_ext)
         
+        nObj = len(cat['FULL SED'].data[:,0])
         
-    cat['GALAXY PROPERTIES'].data['UV_slope'] = uv_slope
-        
-    cat.flush()
-    cat.close()
+        uv_slope = np.zeros(nObj)
+        for i in range(nObj):
+            spec_flambda = cat['FULL SED'].data[i,:]
+
+            #calculate mean flambda in each window
+            #if the wavelengths for the bin edges are not in the wavelength
+            #array, first resample to include them
+            spec_ext_flambda = np.interp(wl_ext, wl, spec_flambda)
+            flambda_windows = np.zeros(10)
+            for j in range(10):
+                flambda_windows[j] = np.mean(spec_ext_flambda[np.all([wl_ext >= binArr[j,0], wl_ext <= binArr[j,1]],axis=0)])
+
+            #fit straight line to these fluxes to derive the UV slope
+
+            a, b, sa, sb, rchi2, dof=lf.linear_fit(np.log10(bin_wl), np.log10(flambda_windows),silent=True)
+            uv_slope[i] = a
+            
+            
+        cat['GALAXY PROPERTIES'].data['UV_slope'] = uv_slope
+            
+        cat.flush()
+        cat.close()
 
