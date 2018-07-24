05ed05963a26d157dbe36b107fcc50aba611e2ac
Jacopo Chevallard <jacopo.chevallard@gmail.com>
2018-07-24 18:05:43 -0300

    Introduced possibility to plot multiple SEDs; added several options for better visualization (see help)

--- bin/plot_Beagle_SED.py
+++ bin/plot_Beagle_SED.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 from autoscale import autoscale_y
+import json 
 
 c_light = 2.99792e+18 # Ang/s
 
@@ -26,6 +27,7 @@ if __name__ == '__main__':
         help="Row in the FULL SED extension to be plotted",
         dest="row", 
         type=int,
+        nargs="+",
         default=0
     )
 
@@ -70,6 +72,35 @@ if __name__ == '__main__':
         default="full sed"
         )
         
+    parser.add_argument(
+        '--json-legend',
+        help="Create a legend from the values in the FITS HDU and FITS column indicated by a JSON string."\
+                " Example: '{\"extName\":\"GALAXY PROPERTIES\", \"colName\":\"max_stellar_age\", \"label\":\"age/yr\"}'.",
+        action="store", 
+        type=str,
+        dest="json_legend" 
+        )
+
+    parser.add_argument(
+        '--normalize',
+        help="Renormalize the SED to median flux value of the SED.",
+        action="store_true", 
+        dest="normalize" 
+        )
+
+    parser.add_argument(
+        '--log-flux',
+        help="Plot logarithmic axis for flux.",
+        action="store_true", 
+        dest="plot_log_flux" 
+        )
+
+    parser.add_argument(
+        '--log-wavelength',
+        help="Plot logarithmic axis for wavelength.",
+        action="store_true", 
+        dest="plot_log_wl" 
+        )
 
 
     args = parser.parse_args()    
@@ -94,6 +125,40 @@ if __name__ == '__main__':
     else:
         sed = SEDs
 
+    # Extract the values used in the legend
+    if args.json_legend is not None:
+        json_leg = obj = json.loads(args.json_legend)
+        try:
+            extName = json_leg["extName"]
+        except:
+            raise ValueError('`extName` must be present in the JSON string (see example).')
+
+        try:
+            colName = json_leg["colName"]
+        except:
+            raise ValueError('`colName` must be present in the JSON string (see example).')
+
+        try:
+            legend_label = json_leg["label"]
+        except:
+            raise ValueError('`label` must be present in the JSON string (see example).')
+
+        legend_values = hdulist[extName].data[colName][args.row]
+
+        if "transformation" in json_leg:
+            for i, x in enumerate(legend_values):
+                x = legend_values[i]
+                legend_values[i] = eval(json_leg['transformation'])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    if args.xrange is not None:
+        ax.set_xlim(args.xrange)
+        loc = np.where((wl >= args.xrange[0]) & (wl <= args.xrange[1]))[0]
+        sed = sed[:, loc]
+        wl = wl[loc]
+
     # Redshift the SED and wl
     if args.redshift is not None:
         sed /= (1.+args.redshift)
@@ -103,11 +168,6 @@ if __name__ == '__main__':
     if args.fnu:
         sed = wl**2/c_light*sed
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-
-    if args.xrange is not None:
-        ax.set_xlim(args.xrange)
 
     ax.set_xlabel('$\lambda / \mu\\textnormal{m}$')
     if args.fnu:
@@ -115,11 +175,34 @@ if __name__ == '__main__':
     else:
         ax.set_ylabel('$f_\lambda / \\textnormal{erg} \, \\textnormal{s}^{-1} \, \\textnormal{cm}^{-2} \, \\textnormal{\AA}^{-1} $')
 
-    ax.plot(wl/wl_factor,
-       sed,
-       lw=1.0,
-       color="red"
-        )
+    for i in range(len(args.row)):
+        _sed = sed[i,:]
+        if args.normalize:
+            wl_center = 0.5*(wl[0]+wl[-1]) ; dwl = wl[-1]-wl[0]
+            loc = np.where((wl >= (wl_center-dwl*0.1)) & (wl <= (wl_center+dwl*0.1)))[0]
+            norm = np.mean(_sed[loc])
+            _sed /= norm
+
+        if args.json_legend is not None:
+            label = legend_label + " = {:.2e}".format(legend_values[i])
+            ax.plot(wl/wl_factor,
+                _sed,
+               lw=1.0,
+               label=label
+                )
+        else:
+            ax.plot(wl/wl_factor,
+               _sed,
+               lw=1.0
+                )
+    if args.json_legend is not None:
+        ax.legend(fontsize=ax.xaxis.get_label().get_fontsize()*0.7)
+
+    if args.plot_log_wl: 
+        ax.set_xscale('log')
+
+    if args.plot_log_flux: 
+        ax.set_yscale('symlog')
 
     autoscale_y(ax)
 
