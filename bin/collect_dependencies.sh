349c7c11cab22338188c0c638bfb83e7825d15e1
Jacopo Chevallard <jacopo.chevallard@mailfence.com>
2025-04-10 15:33:00 +0100

    fix: fixing error 'cp: cannot stat 'not': No such file or directory'

--- bin/collect_dependencies.sh
+++ bin/collect_dependencies.sh
@@ -5,12 +5,23 @@ set -euo pipefail
 FILE_ELF="$1"
 DIR_CONTAINER="$2"
 
-mkdir "$DIR_CONTAINER"
+# Create the container directory if it does not exist
+mkdir -p "$DIR_CONTAINER"
 
-mapfile -t libs < <(ldd "$FILE_ELF" | grep '=>' | awk '{print $3}')
-libs+=($(readelf -l "$FILE_ELF" | grep -Po "(?<=preter:\\s).+(?=\\])"))
+# Collect libraries
+mapfile -t libs < <(ldd "$FILE_ELF" | grep '=>' | awk '{print $3}' | xargs realpath || true)
+libs+=($(readelf -l "$FILE_ELF" 2>/dev/null | grep -Po "(?<=preter:\\s).+(?=\\])" || true))
 
+# Filter out invalid or non-existent paths
+valid_libs=()
 for l in "${libs[@]}"; do
-	mkdir -p "${DIR_CONTAINER}/$(dirname ${l})"
-	cp -L "$l" "${DIR_CONTAINER}/${l}"
+    if [[ -n "$l" && -e "$l" ]]; then
+        valid_libs+=("$l")
+    fi
+done
+
+# Copy libraries to the container directory
+for l in "${valid_libs[@]}"; do
+    mkdir -p "${DIR_CONTAINER}/$(dirname ${l})"
+    cp -L "$l" "${DIR_CONTAINER}/${l}"
 done
